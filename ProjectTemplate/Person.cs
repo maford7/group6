3e9e825afa510efeae62a7a63c8939d40aefba86
Mikayla Ford <mikaylaford@Mikaylas-MacBook-Air.local>
2023-01-28 13:51:29 -0700

    initial setup

---
+++ ProjectTemplate/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collection.Generic;
+using System.linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Person
+    {
+        public string firstname;
+        public string lastname;
+        public int phoneNumber;
+    }
+}
